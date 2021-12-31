16c35188692941d7922bddee85a1eaa1253888a7
NowakArtur97 <nowart97@gmail.com>
2021-12-31 14:32:07 +0100

    Read Mouse Input as world position

--- Assets/Player/Input/PlayerInputManager.cs
+++ Assets/Player/Input/PlayerInputManager.cs
@@ -1,13 +1,24 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
 
 public class PlayerInputManager : MonoBehaviour
 {
     public Vector2 MovementInput { get; private set; }
+    public Vector3 MouseWorldPosition { get; private set; }
 
-    public void Movement(InputAction.CallbackContext context)
+    private Camera _mainCamera;
+    private Vector2Control _currentMousePosition;
+
+    private void Start()
     {
-        MovementInput = context.ReadValue<Vector2>();
-        Debug.Log(MovementInput);
+        _mainCamera = Camera.main;
+        _currentMousePosition = Mouse.current.position;
     }
+
+    public void Movement(InputAction.CallbackContext context) => MovementInput = context.ReadValue<Vector3>();
+
+    private void Update() => ReadMousePosition();
+
+    private void ReadMousePosition() => MouseWorldPosition = _mainCamera.ScreenToWorldPoint(_currentMousePosition.ReadValue());
 }
