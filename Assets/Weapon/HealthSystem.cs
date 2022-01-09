9a24c95af9bd057089a68426cf81448eba75684f
NowakArtur97 <nowart97@gmail.com>
2022-01-09 09:38:28 +0100

    Add death sounds

--- Assets/Weapon/HealthSystem.cs
+++ Assets/Weapon/HealthSystem.cs
@@ -1,8 +1,16 @@
+using System.Collections;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class HealthSystem : MonoBehaviour, IDamagable
 {
     [SerializeField] private float _helath;
+    [SerializeField] float _minSoundPitch = 0.8f;
+    [SerializeField] float _maxSoundPitch = 1.05f;
+
+    private AudioSource _myAudioSource;
+
+    private void Awake() => _myAudioSource = GetComponent<AudioSource>();
 
     public void DealDamage(float damageAmount)
     {
@@ -10,8 +18,23 @@ public class HealthSystem : MonoBehaviour, IDamagable
 
         if (_helath <= 0)
         {
-            // TODO: HeathSystem: Return to pool
-            Destroy(transform.parent.gameObject);
+            PlayDeathSound();
+
+            StartCoroutine(ReleaseCoroutine());
         }
+    }
+
+    private void PlayDeathSound()
+    {
+        _myAudioSource.pitch = Random.Range(_minSoundPitch, _maxSoundPitch);
+        _myAudioSource.Play();
+    }
+
+    private IEnumerator ReleaseCoroutine()
+    {
+        yield return new WaitForSeconds(_myAudioSource.clip.length);
+
+        // TODO: HeathSystem: Return to pool
+        Destroy(transform.parent.gameObject);
     }
 }
