c2ea88ad6356395ce18c0c1caf51218528bcc0c6
NowakArtur97 <nowart97@gmail.com>
2022-01-09 08:13:14 +0100

    Add Projectiles hit sounds

--- Assets/Projectile/Scripts/Projectile.cs
+++ Assets/Projectile/Scripts/Projectile.cs
@@ -1,13 +1,21 @@
+using System.Collections;
 using UnityEngine;
 using static ProjectileObjectPool;
 
+[RequireComponent(typeof(AudioSource))]
 public class Projectile : MonoBehaviour
 {
     [SerializeField] private float _movementVelocity = 20.0f;
     [SerializeField] private float _damageAmount = 10.0f;
     [SerializeField] private ProjectileType _projectileType;
+    [SerializeField] float _minSoundPitch = 0.95f;
+    [SerializeField] float _maxSoundPitch = 1.05f;
 
     private Vector3 _projectileDirection;
+
+    private AudioSource _myAudioSource;
+
+    private void Awake() => _myAudioSource = GetComponent<AudioSource>();
 
     private void Update() => transform.position += _projectileDirection * Time.deltaTime * _movementVelocity;
 
@@ -22,6 +30,16 @@ public class Projectile : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         collision.GetComponentInChildren<IDamagable>()?.DealDamage(_damageAmount);
+
+        _myAudioSource.pitch = Random.Range(_minSoundPitch, _maxSoundPitch);
+        _myAudioSource.Play();
+
+        StartCoroutine(ReleaseCoroutine());
+    }
+
+    private IEnumerator ReleaseCoroutine()
+    {
+        yield return new WaitForSeconds(_myAudioSource.clip.length);
 
         ProjectileObjectPoolInstance.ReleaseProjectile(gameObject, _projectileType);
     }
