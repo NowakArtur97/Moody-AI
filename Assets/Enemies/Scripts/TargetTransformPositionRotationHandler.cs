897510e6c963e53d1081cc4d4fa872b4cc5082d4
NowakArtur97 <nowart97@gmail.com>
2022-01-15 10:59:23 +0100

    Fix issue with target transform in TargetTransformPositionRotationHandler

--- Assets/Enemies/Scripts/TargetTransformPositionRotationHandler.cs
+++ Assets/Enemies/Scripts/TargetTransformPositionRotationHandler.cs
@@ -2,13 +2,15 @@ using UnityEngine;
 
 public class TargetTransformPositionRotationHandler : MonoBehaviour
 {
+    [SerializeField] EnemyTargetTransformHandler _enemyTargetTransformHandler;
+
     private Transform _targetTransform;
 
     private RotationController _rotationController;
 
     private void Awake() => _rotationController = GetComponentInChildren<RotationController>();
 
-    private void Start() => _targetTransform = GetComponent<EnemyTargetTransformHandler>().TargetTransform;
+    private void Start() => _targetTransform = (_enemyTargetTransformHandler ?? GetComponent<EnemyTargetTransformHandler>()).TargetTransform;
 
     private void Update() => HandleRotation();
 
