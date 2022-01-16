6abea5228cddaf4077333e458ab18c2d62b5d2ed
NowakArtur97 <nowart97@gmail.com>
2022-01-16 08:03:41 +0100

    Add Player as Enemies possible attack target

--- Assets/Enemies/Scripts/EnemyRandomTargetTransformManager.cs
+++ Assets/Enemies/Scripts/EnemyRandomTargetTransformManager.cs
@@ -1,8 +1,31 @@
 using UnityEngine;
 public class EnemyRandomTargetTransformManager : MonoBehaviour
 {
+    [SerializeField] private string _playerTag = "Player";
     [SerializeField] private Transform[] _targetTransforms;
+    private Transform _playerTransform;
+
+    public enum TargetType { PLANET, PLAYER }
 
     // TODO: RandomTargetTransformEnemyManager: Add more targets with every ten waves
-    public Transform GetRandomTransform() => _targetTransforms[0];
+    public Transform GetRandomTransform(TargetType targetType)
+    {
+        switch (targetType)
+        {
+            case (TargetType.PLAYER):
+                return FindPlayerTransform();
+            default:
+                return _targetTransforms[0];
+        }
+    }
+
+    private Transform FindPlayerTransform()
+    {
+        if (_playerTransform == null)
+        {
+            _playerTransform = GameObject.FindGameObjectsWithTag(_playerTag)[0].transform;
+        }
+
+        return _playerTransform;
+    }
 }
--- Assets/Enemies/Scripts/EnemyTargetTransformHandler.cs
+++ Assets/Enemies/Scripts/EnemyTargetTransformHandler.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
+using static EnemyRandomTargetTransformManager;
 
 public class EnemyTargetTransformHandler : MonoBehaviour
 {
+    [SerializeField] private TargetType _targetType = TargetType.PLANET;
+
     public Transform TargetTransform { get; private set; }
 
-    private void Start() => TargetTransform = FindObjectOfType<EnemyRandomTargetTransformManager>().GetRandomTransform();
+    private void Start() => TargetTransform = FindObjectOfType<EnemyRandomTargetTransformManager>().GetRandomTransform(_targetType);
 }
