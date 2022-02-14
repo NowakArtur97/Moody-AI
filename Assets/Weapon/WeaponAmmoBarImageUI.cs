12a11481bfde5c59d66522bbabbc8c8eda719bcd
NowakArtur97 <nowart97@gmail.com>
2022-02-14 09:04:34 +0100

    Set Weapon Bar image based on Restoration and Projectile Types

---
+++ Assets/Weapon/WeaponAmmoBarImageUI.cs
@@ -0,0 +1,60 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using static AmmoRestorationManager;
+using static ProjectileObjectPool;
+
+public class WeaponAmmoBarImageUI : MonoBehaviour
+{
+    [SerializeField] private AmmoRestorationType _ammoRestorationType;
+    public AmmoRestorationType RestorationType
+    {
+        get { return _ammoRestorationType; }
+        set { _ammoRestorationType = value; }
+    }
+    [SerializeField] private Sprite _defaultWeaponSprite;
+    [SerializeField] private Sprite _alienWeaponSprite;
+    [SerializeField] private Sprite _rocketLauncherSprite;
+    [SerializeField] private Sprite _mineThrowerSprite;
+    [SerializeField] private Sprite _shurikenThrowerSprite;
+
+    private Image _myImage;
+    private List<WeaponAmmoConsumptionManager> _weaponAmmoConsumptionManager;
+
+    private void Awake()
+    {
+        _myImage = GetComponent<Image>();
+
+        _weaponAmmoConsumptionManager = FindObjectsOfType<WeaponAmmoConsumptionManager>().ToList();
+    }
+
+    private void OnEnable() => SetupImage();
+
+    private void SetupImage()
+    {
+        WeaponAmmoConsumptionManager weaponAmmoConsumptionManager = _weaponAmmoConsumptionManager.Find(manager => manager.RestorationType == _ammoRestorationType);
+
+        if (weaponAmmoConsumptionManager != null)
+        {
+            switch (weaponAmmoConsumptionManager.ProjectileType)
+            {
+                case ProjectileType.DEFAULT_BULLET:
+                    _myImage.sprite = _defaultWeaponSprite;
+                    break;
+                case ProjectileType.BALL_PROJECTILE:
+                    _myImage.sprite = _alienWeaponSprite;
+                    break;
+                case ProjectileType.HOMING_MISSILE:
+                    _myImage.sprite = _rocketLauncherSprite;
+                    break;
+                case ProjectileType.SPIKE_MINE:
+                    _myImage.sprite = _mineThrowerSprite;
+                    break;
+                case ProjectileType.SHURIKEN:
+                    _myImage.sprite = _shurikenThrowerSprite;
+                    break;
+            }
+        }
+    }
+}
--- Assets/Weapon/WeaponAmmoBarsManager.cs
+++ Assets/Weapon/WeaponAmmoBarsManager.cs
@@ -1,14 +1,14 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using static AmmoRestorationManager;
 
 public class WeaponAmmoBarsManager : MonoBehaviour
 {
     [SerializeField] private WeaponAmmoBarUI[] _ammoBars;
+    [SerializeField] private WeaponAmmoBarImageUI[] _ammoBarsImages;
 
     private List<WeaponAmmoBarUI> _weaponAmmoBars;
+    private List<WeaponAmmoBarImageUI> _weaponAmmoBarsImages;
     private WaveManager _waveManager;
     private WaveSpawner _waveSpawner;
     private List<WeaponAmmoConsumptionManager> _weaponAmmoConsumptionManagers;
@@ -16,6 +16,7 @@ public class WeaponAmmoBarsManager : MonoBehaviour
     private void Awake()
     {
         _weaponAmmoBars = _ammoBars.ToList();
+        _weaponAmmoBarsImages = _ammoBarsImages.ToList();
 
         _waveManager = FindObjectOfType<WaveManager>();
         _waveManager.OnStartWave += ShowUsedBars;
@@ -32,10 +33,20 @@ public class WeaponAmmoBarsManager : MonoBehaviour
         _waveSpawner.OnFinishWave -= HideBars;
     }
 
-    private void ShowUsedBars(int waveNumber) =>
+    private void ShowUsedBars(int waveNumber)
+    {
         _weaponAmmoBars
             .ForEach(bar => bar.gameObject
                 .SetActive(_weaponAmmoConsumptionManagers.Exists(manager => manager.RestorationType == bar.RestorationType)));
 
-    private void HideBars() => _weaponAmmoBars.ForEach(bar => bar.gameObject.SetActive(false));
+        _weaponAmmoBarsImages
+            .ForEach(image => image.gameObject
+                .SetActive(_weaponAmmoConsumptionManagers.Exists(manager => manager.RestorationType == image.RestorationType)));
+    }
+
+    private void HideBars()
+    {
+        _weaponAmmoBars.ForEach(bar => bar.gameObject.SetActive(false));
+        _weaponAmmoBarsImages.ForEach(bar => bar.gameObject.SetActive(false));
+    }
 }
