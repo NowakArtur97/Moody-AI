81c16eada4ce30043b74924b3686c319d3ead427
NowakArtur97 <nowart97@gmail.com>
2022-05-07 09:11:46 +0200

    Setup animation script

--- Assets/Player/Animations/PlayerAnimation.cs
+++ Assets/Player/Animations/PlayerAnimation.cs
@@ -1,18 +1,44 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class PlayerAnimation : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private readonly string MOVING_RIGHT_ANIMATION_BOOL_NAME = "isMovingRight";
+    private readonly string MOVING_LEFT_ANIMATION_BOOL_NAME = "isMovingLeft";
+
+    private Animator _myAnimator;
+    private PlayerInputManager _playerInputManager;
+
+    private float _xInput;
+
+    private void Awake()
     {
-        
+        _myAnimator = GetComponent<Animator>();
+
+        _playerInputManager = transform.parent.GetComponentInChildren<PlayerInputManager>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        _xInput = _playerInputManager.MovementInput.x;
+
+        if (_xInput > 0)
+        {
+            SetAnimationBoolVariables(true, false);
+        }
+        else if (_xInput < 0)
+        {
+            SetAnimationBoolVariables(false, true);
+        }
+        else
+        {
+            SetAnimationBoolVariables(false, false);
+        }
+    }
+
+    private void SetAnimationBoolVariables(bool isMovingRight, bool isMovingLeft)
+    {
+        _myAnimator.SetBool(MOVING_RIGHT_ANIMATION_BOOL_NAME, isMovingRight);
+        _myAnimator.SetBool(MOVING_LEFT_ANIMATION_BOOL_NAME, isMovingLeft);
     }
 }
