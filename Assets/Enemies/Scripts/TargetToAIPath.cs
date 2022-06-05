999130fa86d0acd06b3ed38b5f181e3c5e86c352
NowakArtur97 <nowart97@gmail.com>
2022-06-05 09:20:13 +0200

    Setup Jellyfish with Pathfinding

---
+++ Assets/Enemies/Scripts/TargetToAIPath.cs
@@ -0,0 +1,26 @@
+using Pathfinding;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetToAIPath : MonoBehaviour
+{
+    private EnemyTargetTransformHandler _enemyTargetTransformHandler;
+    private AIDestinationSetter _aIDestinationSetter;
+
+    private void Awake()
+    {
+        _enemyTargetTransformHandler = GetComponent<EnemyTargetTransformHandler>();
+        _aIDestinationSetter = GetComponent<AIDestinationSetter>();
+    }
+
+    private void Update() => SetAITarget();
+
+    private void SetAITarget()
+    {
+        if (_aIDestinationSetter.target == null)
+        {
+            _aIDestinationSetter.target = _enemyTargetTransformHandler.FindRandomTargetTransform();
+        }
+    }
+}
