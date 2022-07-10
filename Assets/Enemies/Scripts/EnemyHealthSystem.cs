68485df598226796aae472ba0cd78bd649b0e29c
NowakArtur97 <nowart97@gmail.com>
2022-07-10 09:28:07 +0200

    Fix issue with releasing enemies to object pool

--- Assets/Enemies/Scripts/EnemyHealthSystem.cs
+++ Assets/Enemies/Scripts/EnemyHealthSystem.cs
@@ -7,10 +7,20 @@ public class EnemyHealthSystem : HealthSystem
     [SerializeField] private EnemyType _enemyType;
     [SerializeField] private GameObject _gameObjectToRelease;
 
+    protected override void Awake()
+    {
+        base.Awake();
+
+        if (_gameObjectToRelease == null)
+        {
+            _gameObjectToRelease = transform.parent.gameObject;
+        }
+    }
+
     public override void DeathTrigger()
     {
         AmmoRestorationManagerInstance.RestoreAmmunition(AmmoRestorationType.DEFEATING_ENEMIES);
 
-        EnemyObjectPoolInstance.ReleaseEnemy(_gameObjectToRelease ?? transform.parent.gameObject, _enemyType);
+        EnemyObjectPoolInstance.ReleaseEnemy(_gameObjectToRelease, _enemyType);
     }
 }
