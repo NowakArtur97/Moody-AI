f8759bd1ea0a95d0b513175aa3ed63e5896ddcd3
NowakArtur97 <nowart97@gmail.com>
2022-08-02 14:12:34 +0200

    Spawn Planetoids in random positions in circle

---
+++ Assets/Background/Planetoids/PlanetoidSpawner.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class PlanetoidSpawner : MonoBehaviour
+{
+    [SerializeField] private float _minRadius = 8f;
+    [SerializeField] private float _maxRadius = 13f;
+    [SerializeField] private int _numberOfPlanetoids = 100;
+    [SerializeField] private Vector2 _origin = Vector2.one;
+    [SerializeField] private GameObject[] _planetoids;
+
+    private void Start() => SpawnPlanetoids();
+
+    private void SpawnPlanetoids()
+    {
+        for (int i = 0; i < _numberOfPlanetoids; i++)
+        {
+            Vector2 pointToSpawnAt = RandomPointInAnnulus();
+
+            Instantiate(ChoseRandomPlanetoid(), pointToSpawnAt, Quaternion.identity);
+        }
+    }
+
+    private GameObject ChoseRandomPlanetoid() => _planetoids[Random.Range(0, _planetoids.Length)];
+
+    private Vector2 RandomPointInAnnulus()
+    {
+        Vector2 randomDirection = (Random.insideUnitCircle * _origin).normalized;
+
+        float randomDistance = Random.Range(_minRadius, _maxRadius);
+
+        return _origin + randomDirection * randomDistance;
+    }
+}
