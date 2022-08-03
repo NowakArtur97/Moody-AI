e34299dfe9c1828ba6b1f585790bb77c66c6441f
NowakArtur97 <nowart97@gmail.com>
2022-08-03 12:42:07 +0200

    Add touch damage to Planetoids

--- Assets/Weapon/Scripts/TouchDamage.cs
+++ Assets/Weapon/Scripts/TouchDamage.cs
@@ -4,5 +4,9 @@ public class TouchDamage : MonoBehaviour
 {
     [SerializeField] private float _damageAmount = 10.0f;
 
-    private void OnTriggerEnter2D(Collider2D collision) => GetComponentInChildren<IDamagable>()?.DealDamage(_damageAmount);
+    private IDamagable _healthSystem;
+
+    private void Awake() => _healthSystem = GetComponentInChildren<IDamagable>();
+
+    private void OnTriggerEnter2D(Collider2D collision) => _healthSystem?.DealDamage(_damageAmount);
 }
