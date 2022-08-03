7d77faca115def3e6995735551c0ba5af4d49d54
NowakArtur97 <nowart97@gmail.com>
2022-08-03 12:34:05 +0200

    Fix issue with spawned Planetoids overlapping

---
+++ Assets/Background/Planetoids/Scripts/PlanetoidOverlapCheck.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PlanetoidOverlapCheck : MonoBehaviour
+{
+    [SerializeField] private float _checkRadius = 1.5f;
+    [SerializeField] private LayerMask _layersToCheck;
+
+    private void OnEnable()
+    {
+        List<Collider2D> colliderCheck = FindAllOverlappingObjects();
+
+        if (colliderCheck.Count == 1)
+        {
+            Debug.Log(colliderCheck[0].gameObject.name);
+
+        }
+        if (colliderCheck.Count > 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private List<Collider2D> FindAllOverlappingObjects() =>
+        Physics2D.OverlapCircleAll(transform.position, _checkRadius, _layersToCheck)
+        .Where(collsion => collsion.gameObject != gameObject)
+        .Where(collsion => collsion.transform.parent.gameObject != gameObject)
+        .ToList();
+}
